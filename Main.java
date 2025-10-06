95816e65d083e3beff698b2a4ca2ba3d2f33931b
Nikita <teplii.pled@yandex.ru>
2025-10-06 12:55:58 +0500

    Все классы перенес в один файл

--- Main.java
+++ Main.java
@@ -8,103 +8,243 @@ public class Main {
         switch (theme) {
             case "array":
                 System.out.println("Выбери номер задачи: (task1, task2, task3, task4, task5)");
-                String ArrayTasks = scanner.nextLine();
-                switch (ArrayTasks) {
-                    case "task1":
-                        Tasks.arrayTask1();
+                String arrayTask = scanner.nextLine();
+                switch (arrayTask) {
+                    case "task1": {
+                        System.out.println("Введите X: ");
+                        int x = scanner.nextInt();
+                        int[] arr = { 1, 2, 3, 4, 2, 2, 5 };
+                        Tasks tasks = new Tasks();
+                        int res = tasks.findFirst(arr, x);
+                        if (res >= 0) {
+                            System.out.println("Вхождение числа: " + x + " в массив: " + java.util.Arrays.toString(arr)
+                                    + ": " + res);
+                        } else {
+                            System.out.println("Код ошибки: " + res);
+                        }
                         break;
-                    case "task2":
-                        Tasks.arrayTask2();
+                    }
+                    case "task2": {
+                        int[] arr = { 1, -2, -7, 4, 2, 2, 5 };
+                        Tasks tasks = new Tasks();
+                        int res = tasks.maxAbs(arr);
+                        System.out.println("Максимальное число по модулю: " + res);
                         break;
-                    case "task3":
-                        Tasks.arrayTask3();
+                    }
+                    case "task3": {
+                        System.out.println("Введите позицию X: ");
+                        int x = scanner.nextInt();
+                        Tasks tasks = new Tasks();
+                        int[] arr = { 1, 2, 3, 4, 5 };
+                        int[] ins = { 7, 8, 9 };
+
+                        int[] res = tasks.add(arr, ins, x);
+                        System.out.println("Полученный массив: " + java.util.Arrays.toString(res));
                         break;
-                    case "task4":
-                        Tasks.arrayTask4();
+                    }
+                    case "task4": {
+                        int[] arr = { 1, 2, 3, 4, 5 };
+                        Tasks tasks = new Tasks();
+                        int[] res = tasks.reverseBack(arr);
+                        System.out.println("Массив наоборот: " + java.util.Arrays.toString(res));
                         break;
-                    case "task5":
-                        Tasks.arrayTask5();
+                    }
+                    case "task5": {
+                        System.out.println("Введите значение X: ");
+                        int x = scanner.nextInt();
+                        int[] arr = { 1, 2, 3, 8, 2, 2, 9 };
+                        Tasks tasks = new Tasks();
+                        int[] res = tasks.findAll(arr, x);
+                        System.out.println("Вхождения числа " + x + ": " + java.util.Arrays.toString(res));
                         break;
+                    }
                     default:
                         System.out.println("Нет такой задачи!");
                 }
                 break;
 
             case "method":
+
                 System.out.println("Выбери номер задачи: (task1, task2, task3, task4, task5)");
-                String MethodTasks = scanner.nextLine();
-                switch (MethodTasks) {
-                    case "task1":
-                        Tasks.methodTask1();
+                String methodTask = scanner.nextLine();
+                switch (methodTask) {
+                    case "task1": {
+                        Tasks tasks = new Tasks();
+                        System.out.println("введите значениеы: X");
+
+                        double x = scanner.nextDouble();
+
+                        double res = tasks.fraction(x);
+                        System.out.println(res);
                         break;
-                    case "task2":
-                        Tasks.methodTask2();
+                    }
+                    case "task2": {
+                        Tasks tasks = new Tasks();
+
+                        System.out.println("Введите символ:");
+                        char x = scanner.nextLine().charAt(0); // берем первый символ из строки
+
+                        int res = tasks.charToNum(x);
+                        System.out.println("Символ переведен в число, дорогой пользователь: " + res);
+
+                        scanner.close();
                         break;
-                    case "task3":
-                        Tasks.methodTask3();
+                    }
+                    case "task3": {
+                        Tasks tasks = new Tasks();
+                        System.out.println("Введите значени х:");
+                        int x = scanner.nextInt();
+                        boolean res = tasks.is2Digits(x);
+                        System.out.println("Число двузначное? " + res);
                         break;
-                    case "task4":
-                        Tasks.methodTask4();
+                    }
+                    case "task4": {
+                        Tasks tasks = new Tasks();
+                        System.out.println("Введите значение a: ");
+                        int a = scanner.nextInt();
+                        System.out.println("Введите значение b: ");
+                        int b = scanner.nextInt();
+                        System.out.println("Введите значение num: ");
+                        int num = scanner.nextInt();
+                        boolean res = tasks.isInRange(a, b, num);
+                        System.out.println("Входит ли число в интервал от a до b? " + res);
                         break;
-                    case "task5":
-                        Tasks.methodTask5();
+                    }
+                    case "task5": {
+                        Tasks tasks = new Tasks();
+                        System.out.println("Введите первое число: ");
+                        int one = scanner.nextInt();
+                        System.out.println("Введите второе число: ");
+                        int two = scanner.nextInt();
+                        System.out.println("Введите третье число: ");
+                        int three = scanner.nextInt();
+                        boolean res = tasks.isEqual(one, two, three);
+                        System.out.println("Вывод: " + res);
                         break;
+                    }
                     default:
                         System.out.println("Нет такой задачи!");
                 }
                 break;
 
             case "cycles":
+
                 System.out.println("Выбери номер задачи: (task1, task2, task3, task4, task5)");
-                String CyclesTasks = scanner.nextLine();
-                switch (CyclesTasks) {
-                    case "task1":
-                        Tasks.cyclesTask1();
+                String cyclesTask = scanner.nextLine();
+                switch (cyclesTask) {
+                    case "task1": {
+                        System.out.println("Введите X: ");
+                        Tasks tasks = new Tasks();
+                        int x = scanner.nextInt();
+                        String res = tasks.listNums(x);
+                        System.out.println("Последовательность до X: " + res);
                         break;
-                    case "task2":
-                        Tasks.cyclesTask2();
+                    }
+                    case "task2": {
+
+                        System.out.println("Введите значение X: ");
+                        int x = scanner.nextInt();
+                        Tasks tasks = new Tasks();
+                        String res = tasks.chet(x);
+                        System.out.println("Четная последовательность до X: " + res);
                         break;
-                    case "task3":
-                        Tasks.cyclesTask3();
+                    }
+                    case "task3": {
+                        System.out.println("Введите X: ");
+                        long x = scanner.nextLong();
+                        Tasks tasks = new Tasks();
+                        int res = tasks.numLen(x);
+                        System.out.println("Длина строки: " + res);
                         break;
-                    case "task4":
-                        Tasks.cyclesTask4();
+                    }
+                    case "task4": {
+
+                        System.out.println("Введите Х: ");
+                        int x = scanner.nextInt();
+                        Tasks tasks = new Tasks();
+                        // int res = square.square(x);
+
+                        System.out.println("Квадрат размером " + x + "х" + x + ": ");
+                        tasks.square(x);
                         break;
-                    case "task5":
-                        Tasks.cyclesTask5();
+                    }
+                    case "task5": {
+                        System.out.println("Введите X: ");
+                        int x = scanner.nextInt();
+                        Tasks tasks = new Tasks();
+                        System.out.println("Правый треугольник: ");
+                        tasks.rightTriangle(x);
                         break;
+                    }
                     default:
                         System.out.println("Нет такой задачи!");
                 }
                 break;
 
             case "condition":
+
                 System.out.println("Выбери номер задачи: (task1, task2, task3, task4, task5)");
-                String ConditionTasks = scanner.nextLine();
-                switch (ConditionTasks) {
-                    case "task1":
-                        Tasks.conditionTask1();
+                String conditionTask = scanner.nextLine();
+                switch (conditionTask) {
+                    case "task1": {
+                        Tasks tasks = new Tasks();
+                        System.out.println("Введите значение x:");
+                        int x = scanner.nextInt();
+                        int res = tasks.abs(x);
+                        System.out.println("Вывод: " + res);
                         break;
-                    case "task2":
-                        Tasks.conditionTask2();
+                    }
+                    case "task2": {
+                        System.out.println("Введите значение x: ");
+                        int x = scanner.nextInt();
+                        Tasks tasks = new Tasks();
+                        boolean res = tasks.is35(x);
+                        System.out.println("Вывод: " + res);
                         break;
-                    case "task3":
-                        Tasks.conditionTask3();
+                    }
+                    case "task3": {
+                        Tasks tasks = new Tasks();
+                        System.out.println("Введите значение х: ");
+                        int x = scanner.nextInt();
+
+                        System.out.println("Введите значение y: ");
+                        int y = scanner.nextInt();
+
+                        System.out.println("Введите значение z: ");
+                        int z = scanner.nextInt();
+
+                        int res = tasks.max3(x, y, z);
+                        System.out.println("Максимальное число: " + res);
                         break;
-                    case "task4":
-                        Tasks.conditionTask4();
+                    }
+                    case "task4": {
+                        Tasks tasks = new Tasks();
+                        System.out.println("Введите первое число: X");
+                        int x = scanner.nextInt();
+                        System.out.println("Введите первое число: Y");
+                        int y = scanner.nextInt();
+
+                        int result = tasks.sum2(x, y);
+                        System.out.println("Ответ: " + result);
                         break;
-                    case "task5":
-                        Tasks.conditionTask5();
+                    }
+                    case "task5": {
+                        System.out.println("Введите число: X");
+                        int x = scanner.nextInt();
+                        Tasks tasks = new Tasks();
+
+                        String res = tasks.day(x);
+                        System.out.println("День недели: " + res);
                         break;
+                    }
                     default:
                         System.out.println("Нет такой задачи!");
                 }
                 break;
+
             default:
                 System.out.println("Нет такой темы!");
         }
-
         scanner.close();
     }
 }
--- Tasks.java
+++ Tasks.java
@@ -1,237 +1,225 @@
 import java.util.Scanner;
 
 public class Tasks {
-    public static void methodTask1() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("введите значение: X");
-        double x = scanner.nextDouble();
-        double res = x - (int)x;
-        System.out.println(res);
-        scanner.close();
-    }
 
-    public static void methodTask2() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите символ:");
-        char x = scanner.nextLine().charAt(0);
-        int res = (int)x;
-        System.out.println("Символ переведен в число: " + res);
-        scanner.close();
-    }
-
-    public static void methodTask3() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение x:");
-        int x = scanner.nextInt();
-        boolean res = (x >= 10 && x <= 99) || (x <= -10 && x >= -99);
-        System.out.println("Число двузначное? " + res);
-        scanner.close();
-    }
-
-    public static void methodTask4() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение a: ");
-        int a = scanner.nextInt();
-        System.out.println("Введите значение b: ");
-        int b = scanner.nextInt();
-        System.out.println("Введите значение num: ");
-        int num = scanner.nextInt();
-        boolean res = num >= a && num <= b;
-        System.out.println("Входит ли число в интервал от a до b? " + res);
-        scanner.close();
-    }
-
-    public static void methodTask5() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int one = scanner.nextInt();
-        System.out.println("Введите второе число: ");
-        int two = scanner.nextInt();
-        System.out.println("Введите третье число: ");
-        int three = scanner.nextInt();
-        boolean res = (one == two) && (two == three);
-        System.out.println("Вывод: " + res);
-        scanner.close();
-    }
-
-    public static void cyclesTask1() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите X: ");
-        int x = scanner.nextInt();
-        StringBuilder res = new StringBuilder();
-        for (int i = 1; i <= x; i++) {
-            res.append(i).append(" ");
-        }
-        System.out.println("Последовательность до X: " + res.toString().trim());
-        scanner.close();
-    }
-
-    public static void cyclesTask2() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение X: ");
-        int x = scanner.nextInt();
-        StringBuilder res = new StringBuilder();
-        for (int i = 2; i <= x; i += 2) {
-            res.append(i).append(" ");
-        }
-        System.out.println("Четная последовательность до X: " + res.toString().trim());
-        scanner.close();
-    }
-
-    public static void cyclesTask3() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите X: ");
-        long x = scanner.nextLong();
-        int res = String.valueOf(x).length();
-        System.out.println("Длина строки: " + res);
-        scanner.close();
-    }
-
-    public static void cyclesTask4() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите X: ");
-        int x = scanner.nextInt();
-        System.out.println("Квадрат размером " + x + "x" + x + ": ");
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < x; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        scanner.close();
-    }
-
-    public static void cyclesTask5() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите X: ");
-        int x = scanner.nextInt();
-        System.out.println("Правый треугольник: ");
-        for (int i = 1; i <= x; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        scanner.close();
-    }
-
-    public static void arrayTask1() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите X: ");
-        int x = scanner.nextInt();
-        int[] arr = {1,2,3,4,2,2,5};
-        int res = -1;
+    public int findFirst(int[] arr, int x) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
-                res = i;
-                break;
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    public int maxAbs(int[] arr) {
+        int origMaxabs = arr[0];
+        int maxabs = Math.abs(arr[0]);
+        for (int i = 0; i < arr.length; i++) {
+            int currentabc = Math.abs(arr[i]);
+            if ((currentabc) >= (maxabs)) {
+                origMaxabs = arr[i];
             }
         }
-        if (res >= 0) {
-            System.out.println("Вхождение числа: " + x + " в массив: " + java.util.Arrays.toString(arr) + ": " + res);
-        } else {
-            System.out.println("Код ошибки: " + res);
+        return origMaxabs;
+    }
+
+    public int[] add(int[] arr, int[] ins, int pos) {
+        int[] resEnd = new int[arr.length + ins.length];
+        int index = 0;
+
+        for (int i = 0; i < pos; i++) {
+            resEnd[index] = arr[i];
+            index++;
         }
-        scanner.close();
-    }
-
-    public static void arrayTask2() {
-        int[] arr = {1,-2,-7,4,2,2,5};
-        int maxAbs = Math.abs(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (Math.abs(arr[i]) > maxAbs) {
-                maxAbs = Math.abs(arr[i]);
-            }
+        for (int i = 0; i < ins.length; i++) {
+            resEnd[index] = ins[i];
+            index++;
         }
-        System.out.println("Максимальное число по модулю: " + maxAbs);
+        for (int i = pos; i < arr.length; i++) {
+            resEnd[index] = arr[i];
+            index++;
+        }
+
+        return resEnd;
     }
 
-    public static void arrayTask3() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите позицию X: ");
-        int x = scanner.nextInt();
-        int[] arr = {1, 2, 3, 4, 5};
-        int[] ins = {7, 8, 9};
-        int[] res = new int[arr.length + ins.length];
-        for (int i = 0; i < x; i++) res[i] = arr[i];
-        for (int i = 0; i < ins.length; i++) res[x + i] = ins[i];
-        for (int i = x; i < arr.length; i++) res[i + ins.length] = arr[i];
-        System.out.println("Полученный массив: " + java.util.Arrays.toString(res));
-        scanner.close();
-    }
-
-    public static void arrayTask4() {
-        int[] arr = {1,2,3,4,5};
+    public int[] reverseBack(int[] arr) {
         int[] res = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) res[i] = arr[arr.length - 1 - i];
-        System.out.println("Массив наоборот: " + java.util.Arrays.toString(res));
+        for (int i = arr.length - 1; i >= 0; i--) {
+            res[arr.length - 1 - i] = arr[i];
+        }
+        return res;
     }
 
-    public static void arrayTask5() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение X: ");
-        int x = scanner.nextInt();
-        int[] arr = {1,2,3,8,2,2,9};
-        int[] temp = new int[arr.length];
+    public int[] findAll(int[] arr, int x) {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == x) temp[count++] = i;
+            if (arr[i] == x) {
+                count++;
+            }
         }
+
         int[] res = new int[count];
-        System.arraycopy(temp, 0, res, 0, count);
-        System.out.println("Вхождения числа " + x + ": " + java.util.Arrays.toString(res));
-        scanner.close();
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                res[index] = i;
+                index++;
+            }
+        }
+        return res;
+
     }
 
-    public static void conditionTask1() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение x:");
-        int x = scanner.nextInt();
-        int res = Math.abs(x);
-        System.out.println("Вывод: " + res);
-        scanner.close();
+    public double fraction(double x) {
+        double y = x - Math.floor(x);
+        return y;
     }
 
-    public static void conditionTask2() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение x: ");
-        int x = scanner.nextInt();
-        boolean res = (x % 3 == 0) || (x % 5 == 0);
-        System.out.println("Вывод: " + res);
-        scanner.close();
+    public int charToNum(char x) {
+        char zero = '0';
+        int res = x - zero;
+        return res;
     }
 
-    public static void conditionTask3() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение x: ");
-        int x = scanner.nextInt();
-        System.out.println("Введите значение y: ");
-        int y = scanner.nextInt();
-        System.out.println("Введите значение z: ");
-        int z = scanner.nextInt();
-        int res = Math.max(x, Math.max(y, z));
-        System.out.println("Максимальное число: " + res);
-        scanner.close();
+    public boolean is2Digits(int x) {
+        if ((x >= 10) && (x <= 99)) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
-    public static void conditionTask4() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число: X");
-        int x = scanner.nextInt();
-        System.out.println("Введите второе число: Y");
-        int y = scanner.nextInt();
-        int result = x + y;
-        System.out.println("Ответ: " + result);
-        scanner.close();
+    public boolean isInRange(int a, int b, int num) {
+
+        if (a < b) {
+            if ((a <= num) && (num <= b)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if ((a >= num) && (num >= b)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
     }
 
-    public static void conditionTask5() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число: X");
-        int x = scanner.nextInt();
-        String[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
-        String res = (x >= 1 && x <= 7) ? days[x - 1] : "Нет такого дня";
-        System.out.println("День недели: " + res);
-        scanner.close();
+    public boolean isEqual(int a, int b, int c) {
+        if ((a == b) && (b == c) && (a == c)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String listNums(int x) {
+        String[] a = new String[x + 1];
+        for (int i = 0; i < x; i++) {
+            a[i] = String.valueOf(i);
+        }
+        return String.join(" ", a);
+    }
+
+    public String chet(int x) {
+        String a[] = new String[(x / 2) + 1];
+        int index = 0;
+        for (int i = 0; i <= x; i += 2) {
+            a[index] = String.valueOf(i);
+            index++;
+        }
+        return String.join(" ", a);
+    }
+
+    public int numLen(long x) {
+        String a = String.valueOf(x);
+        int len = a.length();
+        return len;
+
+    }
+
+    public void square(int x) {
+        // StringBuilder sq = new StringBuilder();
+        for (int i = 0; i < x; i++) {
+            for (int q = 0; q < x; q++) {
+                // sq.append(" * ");
+                System.out.print("* ");
+            }
+            // sq.append("\n");
+            System.out.println(" ");
+        }
+
+        // System.out.print(sq.toString());
+    }
+
+    public void rightTriangle(int x) {
+        int c = 1;
+        for (int i = 0; i < x; i++) {
+            for (int q = 0; q < c; q++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+            c = c + 1;
+        }
+    }
+
+    public int abs(int x) {
+        return Math.abs(x);
+    }
+
+    public boolean is35(int x) {
+        if (((x % 5) == 0) && ((x % 3) == 0)) {
+            return false;
+        } else if (((x % 5) == 0) || (x % 3) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public int max3(int x, int y, int z) {
+        if ((x >= y) && (x >= z)) {
+            return x;
+        } else if ((y >= x) && (y >= z)) {
+            return y;
+        } else {
+            return z;
+        }
+    }
+
+    public int sum2(int x, int y) {
+        int res = x + y;
+        if ((10 <= res) && (res <= 19)) {
+            return 20;
+        } else {
+            return res;
+        }
+    }
+
+    public String day(int x) {
+        switch (x) {
+            case 1:
+                return "Понедельник";
+            case 2:
+                return "Вторник";
+            case 3:
+                return "Среда";
+            case 4:
+                return "Четверг";
+            case 5:
+                return "Пятница";
+            case 6:
+                return "Суббота";
+            case 7:
+                return "Воскресенье";
+            default:
+                return "Ошибка, введите значение от 1 до 7";
+        }
     }
 }
